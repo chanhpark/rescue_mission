0ed0680bb0f4f58f4d13146e4aef17f73381574f
chanhpark <chanhyun.park1211@gmail.com>
2014-12-15 14:03:49 -0500

    1st passing test to visit /posts

--- spec/features/user_creates_a_post_spec.rb
+++ spec/features/user_creates_a_post_spec.rb
@@ -3,16 +3,14 @@ require 'rails_helper'
 feature "user creates a post", %q{
 As a user
 I want to post a question
-So that I can receive help from others
-}
+So that I can receive help from others } do
   # Acceptance Criteria
   # - I must provide a title that is at least 40 characters long
   # - I must provide a description that is at least 150 characters long
-  # - I must be presented with errors if I fill out the form incorrectly
-context "visit page" do
-  it "creates a post with valid attributes" do
+#   # - I must be presented with errors if I fill out the form incorrectly
+# context "visit page" do
+  it "visit page" do
     visit '/posts'
-    visit '/posts/new'
-    expect(page).to have_content "this is where you post"
+    expect(page).to have_content "where you post a new issue"
   end
 end
